fe3f1d882717ea447023d17047294698cf5cd947
Nahom Mehanzel <nknahom@gmail.com>
2021-08-03 02:28:55 +0000

    Add new form template and setup a new route for both GET and POST to that fiel

--- express_server.js
+++ express_server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -16,6 +23,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
