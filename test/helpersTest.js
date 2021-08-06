30d145e143a045b99036e92c79709a15815e9c34
Nahom Mehanzel <nknahom@gmail.com>
2021-08-06 03:19:24 +0000

    Unit tests passed!

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,13 @@
 const { assert } = require("chai");
 
-const { getUserByEmail, checkEmailAndPassword } = require("../helpers.js");
+const {
+  getUserByEmail,
+  checkEmailAndPassword,
+  getUserById,
+  isLoggedIn,
+  urlsForUser,
+  isShortURLExist,
+} = require("../helpers.js");
 
 const testUsers = {
   userRandomID: {
@@ -15,6 +22,12 @@ const testUsers = {
     // 'pass2' is the password before hashed
     password: "$2b$10$2lLnqqe9isMgb4P6N892hu2.1CV1ZIHfKZh5aQyQscylPjV4bRObu",
   },
+};
+
+const urlDatabase = {
+  b2xVn2: { userID: 1, longURL: "http://www.lighthouselabs.ca" },
+  "9sm5xK": { userID: 1, longURL: "http://www.google.com" },
+  d34565: { userID: 2, longURL: "http://www.google.com" },
 };
 
 describe("getUserByEmail", () => {
@@ -41,5 +54,61 @@ describe("checkEmailAndPassword", () => {
   it("should return false if Email & Password don't exist", () => {
     const password = "pass";
     assert.equal(checkEmailAndPassword("a@a.com", password, testUsers), false);
+  });
+});
+
+describe("getUserById", () => {
+  it("should return a user with valid id", () => {
+    const user = getUserById("userRandomID", testUsers);
+    const expectedOutput = "userRandomID";
+    assert.strictEqual(user.id, expectedOutput);
+  });
+
+  it("should return a null with invalid id", () => {
+    const user = getUserById("userRandomID3", testUsers);
+    const expectedOutput = null;
+    assert.strictEqual(user, expectedOutput);
+  });
+
+  it("should return a null with invalid input", () => {
+    const user = getUserById();
+    const expectedOutput = null;
+    assert.strictEqual(user, expectedOutput);
+  });
+});
+
+describe("isLoggedIn", () => {
+  it("should return true if logged in", () => {
+    const actualOutput = isLoggedIn("userRandomID", testUsers);
+    const expectedOutput = true;
+    assert.strictEqual(actualOutput, expectedOutput);
+  });
+
+  it("should return false if not logged in", () => {
+    const actualOutput = isLoggedIn("4", testUsers);
+    const expectedOutput = false;
+    assert.strictEqual(actualOutput, expectedOutput);
+  });
+});
+
+describe("urlsForUser", () => {
+  it("should return true if user includes the id", () => {
+    const actualOutput = urlsForUser("2", urlDatabase);
+    const expectedOutput = { d34565: urlDatabase["d34565"] };
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+});
+
+describe("isShortURLExist", () => {
+  it("should return false if user have access permission", () => {
+    const actualOutput = isShortURLExist("b2xVn2", 2, urlDatabase);
+    const expectedOutput = false;
+    assert.strictEqual(actualOutput, expectedOutput);
+  });
+
+  it("should return true if user have access permission", () => {
+    const actualOutput = isShortURLExist("b2xVn2", 1, urlDatabase);
+    const expectedOutput = true;
+    assert.strictEqual(actualOutput, expectedOutput);
   });
 });
